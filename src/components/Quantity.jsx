846bba5e6868b49391a4015bbf434ae0da11279e
Austeja <sketeryteausteja@gmail.com>
2024-10-02 15:31:55 +0200

    Update icon styles

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -17,9 +17,9 @@ const Quantity = ({ itemCount, setItemCount }) => {
 
     return (
         <div className='product__quantity'>
-            <button className='product__quantity__btn' onClick={() => handleCount('decrease')} style={itemCount === 0 ? {color: '#CAC9C9'} : {}}><FaMinus style={{color: '#000000'}} /></button>
+            <button className='product__quantity__btn' onClick={() => handleCount('decrease')} style={itemCount === 0 ? {color: '#CAC9C9'} : {}}><FaMinus /></button>
             <p className='product__quantity__value'>{itemCount}</p>
-            <button className='product__quantity__btn' onClick={() => handleCount('increase')}><FaPlus style={{color: '#000000'}} /></button>
+            <button className='product__quantity__btn' onClick={() => handleCount('increase')}><FaPlus /></button>
         </div>
     )
 }
