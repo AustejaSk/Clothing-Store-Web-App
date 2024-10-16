352d737d7e0343a32ad30fa19199a1718e309b74
Austeja <sketeryteausteja@gmail.com>
2024-10-16 14:25:42 +0200

    Update Account.jsx, update styles

--- src/pages/Accout.jsx
+++ src/pages/Accout.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import PageHeader from '../components/PageHeader'
+
+import customerImg from '../images/customer.png'
+import settingsIcon from '../images/settings.png'
+import ordersIcon from '../images/orders.png'
+import accountIcon from '../images/profile.png'
 
 const Account = () => {
     return (
-        <h1>Account Page</h1>
+        <div className='account'>
+            <PageHeader name='Account' />
+            <div className='customerInfo'>
+                <img className='customerInfo__img' src={customerImg} alt='Profile image.' />
+                <h1 className='customerInfo__name'>Emily Carter</h1>
+            </div>
+            <ul className='accountMenu'>
+                <Link to='.' className='accountMenu__option'><img className='accountMenu__optionIcon' src={settingsIcon} alt="Settings" />Settings</Link>
+                <Link to='.' className='accountMenu__option'><img className='accountMenu__optionIcon' src={accountIcon} alt="Account" />Account</Link>
+                <Link to='.' className='accountMenu__option'><img className='accountMenu__optionIcon' src={ordersIcon} alt="Order history" />Order history</Link>
+            </ul>
+            <button className='signout-btn'>Sign out</button>
+        </div>
     )
 }
 
